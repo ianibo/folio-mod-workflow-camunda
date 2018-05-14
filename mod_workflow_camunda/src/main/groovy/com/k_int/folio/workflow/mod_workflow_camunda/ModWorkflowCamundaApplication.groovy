dbc66c7729ecb1eebe11514943a87480069431cf
Ian Ibbotson <ianibbo@gmail.com>
2018-05-14 18:02:51 +0100

    updated

--- mod_workflow_camunda/src/main/groovy/com/k_int/folio/workflow/mod_workflow_camunda/ModWorkflowCamundaApplication.groovy
+++ mod_workflow_camunda/src/main/groovy/com/k_int/folio/workflow/mod_workflow_camunda/ModWorkflowCamundaApplication.groovy
@@ -1,12 +1,19 @@
 package com.k_int.folio.workflow.mod_workflow_camunda
 
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.camunda.bpm.engine.RuntimeService;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@SpringBootApplication
-class ModWorkflowCamundaApplication {
+public class ModWorkflowCamundaApplication implements InitializingBean {
 
-  static void main(String[] args) {
-    SpringApplication.run ModWorkflowCamundaApplication, args
+  @Autowired
+  private RuntimeService runtimeService;
+
+  public void afterPropertiesSet() throws Exception {
+    runtimeService.startProcessInstanceByKey("loanApproval");
+  }
+
+  public void setRuntimeService(RuntimeService runtimeService) {
+    this.runtimeService = runtimeService;
   }
 }
