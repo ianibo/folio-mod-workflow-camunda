2c2d6120794e41ffc4b20cf006649329a6e2b6f4
Ian Ibbotson <ianibbo@gmail.com>
2018-05-17 17:38:50 +0100

    Trying different strategy for multi tenant schema creation - now seeing table already exists. create seems to be ignoring the prefix option

--- mod_workflow_camunda/src/main/groovy/com/k_int/folio/workflow/mod_workflow_camunda/ModWorkflowCamundaApplication.groovy
+++ mod_workflow_camunda/src/main/groovy/com/k_int/folio/workflow/mod_workflow_camunda/ModWorkflowCamundaApplication.groovy
@@ -3,16 +3,24 @@ package com.k_int.folio.workflow.mod_workflow_camunda
 import org.camunda.bpm.engine.RuntimeService;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.camunda.bpm.engine.spring.SpringProcessEngineConfiguration;
+import org.camunda.bpm.engine.spring.SpringProcessEngineConfiguration;
 import org.camunda.bpm.engine.ProcessEngineConfiguration;
 import org.camunda.bpm.container.RuntimeContainerDelegate;
 import org.camunda.bpm.engine.ProcessEngine
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy
+import org.springframework.jdbc.datasource.DataSourceTransactionManager
 
 
 public class ModWorkflowCamundaApplication implements InitializingBean {
 
   @Autowired
   private RuntimeService runtimeService;
+ 
+  @Autowired
+  private TransactionAwareDataSourceProxy dataSource
+
+  @Autowired
+  private DataSourceTransactionManager transactionManager
 
   // @Autowired
   // private SpringProcessEngineConfiguration processEngineConfiguration;
@@ -28,22 +36,37 @@ public class ModWorkflowCamundaApplication implements InitializingBean {
   }
 
   private void configureTestTenants() {
-    println("ModWorkflowCamundaApplication::configureTestTenants");
+    println("\n\n****\n\nModWorkflowCamundaApplication::configureTestTenants\n\n****\n\n");
     // https://docs.camunda.org/manual/7.7/user-guide/spring-framework-integration/
     // 
     // Consider using .setDataSource(dataSource());
+    // .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE)
     //
-    ProcessEngine processEngine = ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration()
-       .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE)
-       .setJdbcDriver('org.postgresql.Driver')
-       .setJdbcUrl('jdbc:postgresql://pghost:5432/foliodev')
-       .setJdbcUsername('folio')
-       .setJdbcPassword('folio')
-       .setDatabaseTablePrefix('modwf_diku.')
-       .setJobExecutorActivate(true)
-       .setProcessEngineName('diku')
-       .setHistory(ProcessEngineConfiguration.HISTORY_FULL)
-       .buildProcessEngine();
+    // https://docs.camunda.org/manual/7.3/api-references/deployment-descriptors/#tags-process-engine-configuration-configuration-properties
+    //
+    // ProcessEngine processEngine = ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration()
+    // .setJdbcUrl('jdbc:postgresql://pghost:5432/foliodev?currentSchema=modwf_diku')
+    // .setJdbcDriver('org.postgresql.Driver')
+    // .setJdbcUrl('jdbc:postgresql://pghost:5432/foliodev')
+    // .setJdbcUsername('folio')
+    // .setJdbcPassword('folio')
+
+    SpringProcessEngineConfiguration config = new SpringProcessEngineConfiguration();
+
+    println("dataSource: ${dataSource}");
+    println("transactionManager: ${transactionManager}");
+
+    config.setDatabaseSchemaUpdate('true')
+    config.setDatabaseTablePrefix('modwf_diku.')
+    config.setJobExecutorActivate(true)
+    config.setProcessEngineName('diku')
+    config.setHistory(ProcessEngineConfiguration.HISTORY_FULL)
+    config.setDataSource(dataSource)
+    config.setTransactionManager(transactionManager)
+
+    println("config: ${config}");
+
+    ProcessEngine processEngine = config.buildProcessEngine();
   
     RuntimeContainerDelegate.INSTANCE.get().registerProcessEngine(processEngine);
 
